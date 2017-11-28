3bd3a16f4e1c3f3153e705be63cd72aa2c3a3dc0
Sebastian Kopf <seb.kopf@gmail.com>
2017-11-28 15:28:03 -0700

    skeleton for the read_svm function

---
+++ R/svm_data.R
@@ -0,0 +1,10 @@
+#' Read SVM data file
+#' @description This functions reads the data file
+#' @param filepath the path to the svm data file
+#' @return data_frame with the read data
+#' @export
+read_svm_data_file <- function(filepath) {
+
+  #read_excel(filepath)
+
+}
