ce2b4600d60310a0f3d2ffba107aedbd732a0840
Sebastian Kopf <seb.kopf@gmail.com>
2017-11-28 16:14:12 -0700

    finalize the read function for svm files

--- R/package.R
+++ R/package.R
@@ -1,7 +1,7 @@
 #' @keywords internal
 "_PACKAGE"
 
-#' @importFrom readr read_csv
+#' @importFrom readr read_csv cols_only col_character col_double
 #' @importFrom dplyr as_data_frame %>%
 #' @importFrom glue glue collapse
 NULL
--- tests/testthat/test-svm-files.R
+++ tests/testthat/test-svm-files.R
@@ -4,5 +4,10 @@ test_that("Testing read functions", {
 
   expect_error(read_svm_data_file("DNE"), "svm file does not seem to exist")
 
+  # read a good test file
+  expect_true(is.data.frame(svm_data <- read_svm_data_file(file.path("test_data", "svm_pred_results.csv"))))
+  expect_equal(nrow(svm_data), 101721)
 
+  # check for warnings in a bad test file
+  expect_warning(read_svm_data_file(file.path("test_data", "svm_pred_results_bad.csv")), "don't match the column names")
 })
