f8806e7652f47be06b4b8afbb0fad231c1644ea5
Sebastian Kopf <seb.kopf@gmail.com>
2017-11-28 17:49:38 -0700

    documentation and imports updated

--- R/package.R
+++ R/package.R
@@ -2,6 +2,6 @@
 "_PACKAGE"
 
 #' @importFrom readr read_csv cols_only col_character col_double
-#' @importFrom dplyr as_data_frame %>%
+#' @importFrom dplyr as_data_frame %>% filter mutate
 #' @importFrom glue glue collapse
 NULL
