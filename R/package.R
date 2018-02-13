3b9c8a67b9cf50ca5fef603941e8d0515e06b28f
Cwalsh <corinneinnes@Corinnes-MacBook-Pro.local>
2018-02-13 14:44:07 -0700

    package updates - making sparql work

--- R/package.R
+++ R/package.R
@@ -2,11 +2,13 @@
 "_PACKAGE"
 
 #' @importFrom rlang !!
-#' @importFrom readr read_csv cols_only col_character col_double
+#' @importFrom readr read_csv cols_only col_character col_double read_rds write_rds
 #' @importFrom readxl read_excel
 #' @importFrom dplyr as_data_frame %>% filter mutate data_frame left_join select rename bind_rows
 #' @importFrom glue glue collapse
 #' @importFrom purrr safely map map2 map_lgl map_dbl map_int
 #' @importFrom tidyr nest unnest
 #' @importFrom broom tidy glance
+#' @import SPARQL
+#' @import RCurl
 NULL
